f2030c866123ba318140ec2c219efb2316541b26
GodSamble <105866831+GodSamble@users.noreply.github.com>
2023-02-05 18:00:58 +0000

    greedy5

--- algorithm/greedy5.py
+++ algorithm/greedy5.py
@@ -1,8 +1,20 @@
 n=int(input())
-roads=list(map(int,input().split()))
-costs=list(map(int,input().split()))
+roads=list(map(int,input().split())) #1234
+costs=list(map(int,input().split())) #4 2 5 6
 
-res=roads[0]*costs[0]
-m=costs[0]
-dist=0
+res=roads[0]*costs[0] #얘의 초기값은 고정값으로 세팅하고 추가되는 때마다 리뉴얼
+#하고, 최종적으로 print(res)할 것임.
+m=costs[0] # 저장고
+dist=0  #
 
+for i in range(1,n-1):
+    if costs[i]<m:
+      res+=m*dist
+      dist=roads[i] #
+      m=costs[i] #
+    else:
+      dist+=roads[i] #
+
+    if i==n-2:
+      res+=m*dist
+print(res)
